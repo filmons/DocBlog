28cad8e3a40169e6f95c235f236f4c92a56d9626
filmons <filmonseare@gmail.com>
2022-10-15 19:35:57 +0200

    adding article page and the pagination dynamique

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import index
+from .views import index, article
 
 urlpatterns = [
-    path("", index, name="blog-homepage")
+    path("", index, name="blog-homepage"),
+    path("article_<str:numero_article>/", article, name="blog-article")
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "blog/index.html", context={"date": datetime.today()})
+
+
+def article(request, numero_article):
+    if numero_article in ["01", "02" , "03"]:
+        return render(request, f"blog/article_{numero_article}.html")
+    return render(request, 'blog/article_not_found.html')
